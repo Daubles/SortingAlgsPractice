174cee2eb9f5be1c88a69b8a9f7a425c718dd5f3
Jamie Cavanaugh <JamieCavanaugh@co-operativebank.co.nz>
2022-10-06 13:42:23 +1300

    Add initial logging and host configuration

---
+++ SortingAlgsPractice/Builders/LoggingConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace SortingAlgsPractice.Builders;
+
+public static class LoggingConfiguration
+{
+    public static void ConfigureLogging(
+        this LoggerConfiguration loggerConfiguration,
+        IServiceProvider services,
+        IConfiguration configuration)
+    {
+        loggerConfiguration
+            .MinimumLevel.ControlledBy(new LoggingLevelSwitch(LogEventLevel.Debug))
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+        
+        loggerConfiguration.WriteTo.Console(LogEventLevel.Debug);
+    }
+}
